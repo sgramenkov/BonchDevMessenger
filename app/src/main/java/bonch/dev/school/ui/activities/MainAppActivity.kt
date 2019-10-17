82c4d8fda59a43165ea1d049afc84c60a5fcd0df
sgramenkov <56446832+sgramenkov@users.noreply.github.com>
2019-10-17 22:11:13 +0300

    Start!

---
+++ app/src/main/java/bonch/dev/school/ui/activities/MainAppActivity.kt
@@ -0,0 +1,13 @@
+package bonch.dev.school.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bonch.dev.school.R
+
+class MainAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_app)
+    }
+}
---
+++ app/src/main/java/bonch/dev/school/ui/fragments/ChatFragment.kt
@@ -0,0 +1,6 @@
+package bonch.dev.school.ui.fragments
+
+import androidx.fragment.app.Fragment
+
+class ChatFragment:Fragment() {
+}
